5c760559b32cbd97e8b43f714d6b1bbd10161cf5
root <root@ip-172-31-48-145.ec2.internal>
2020-08-19 19:57:02 +0000

    Add the code to delete the backup files olders than 2 days.

--- post_dump_cron.sh
+++ post_dump_cron.sh
@@ -2,8 +2,13 @@
 
 set -x
 
-docker exec postgres_host pg_dump -U postgres -F t stage_db | gzip > /var/lib/docker/db_backup/dump_`date +%d-%m-%Y"_"%H_%M_%S`.tar.gz
+cd /home/ubuntu/db_backup/
 
-cd /var/lib/docker/db_backup/
+# remove the backup files older than 2 days
+find ./ -name "*.gz" -type f -mtime +2 -exec git rm {} \;
+
+# dump the data backup file to current directory
+docker exec postgres_host pg_dump -U postgres -F t stage_db | gzip > $PWD/dump_`date +%d-%m-%Y"_"%H_%M_%S`.tar.gz
+
 git add . && git commit -m "dump data of `date`" 
 git status && git push origin master
